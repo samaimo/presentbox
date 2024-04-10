27dc4c99038e39e9435fce7a515bce637a58a979
samaimo <kii328815@gmail.com>
2024-04-10 17:53:14 +0900

    Boxモデルのバリデーション記述

--- app/models/box.rb
+++ app/models/box.rb
@@ -1,5 +1,10 @@
 class Box < ApplicationRecord
   has_one_attached :image
   belongs_to :user
-  has_many :presents
+  has_many :presents, dependent: :destroy
+
+  # バリデーション
+  # 入力必須
+  validates :box_name, presence: true
+  validates :image, presence: { message: 'を選択してください' }
 end
