7c2ccf52661cbc0304d9757d1e99e920c988f844
samaimo <kii328815@gmail.com>
2024-04-12 09:54:36 +0900

    Revert "バリデーションの記述"
    This reverts commit eafecc58bfb591177dfc78f9732e8b31f13ec5e5.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :boxes
   has_many :presents
-
-  # パスワードは英数字混合の入力が必須
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
-
-  # 全角（かな・カナ・漢字）または半角英字が必須
-  validates :nickname, presence: true,
-                       format: { with: /\A[ぁ-んァ-ン一-龠々あ-㐂ａ-ｚＡ-Ｚ]+$\z/ }
 end
