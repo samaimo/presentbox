682f22ea10659c410ebd36f1fa9dbb371fd30891
samaimo <kii328815@gmail.com>
2024-04-13 08:36:11 +0900

    日付の表示変更
    【変更前】XX-XX-XX
    【変更後】XX.XX.XX

--- app/models/present.rb
+++ app/models/present.rb
@@ -7,4 +7,9 @@ class Present < ApplicationRecord
   # 入力必須
   validates :title, :present_name, presence: true
   validates :image, presence: { message: 'を選択してください' }
+
+  # event_dateというカラムに保存された日付を取得するメソッド
+  def formatted_date
+    event_date.strftime('%Y. %-m. %-d') if event_date.present?
+  end
 end
