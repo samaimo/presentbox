990461a133dad20da311c1ecc32d7cc254b08025
samaimo <kii328815@gmail.com>
2024-04-16 09:38:03 +0900

    バリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,6 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
-  # 全角（かな・カナ・漢字）または半角英字が必須
   validates :nickname, presence: true,
-                       format: { with: /\A[ぁ-んァ-ン一-龠々あ-㐂ａ-ｚＡ-Ｚ]+$\z/ }
+                       format: { with: /\A[ぁ-んァ-ン一-龠々a-zA-Z]+\z/, message: 'は全角かな・カナ・漢字または半角英字で入力してください' }
 end
