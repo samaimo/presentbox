a81e11f8d77ff4e339a90b6c74371c0dc7f822a5
samaimo <kii328815@gmail.com>
2024-04-19 09:03:11 +0900

    コントローラー設定

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,3 +1,24 @@
 class MemosController < ApplicationController
+  def index
+    @memos = Memo.all
+  end
 
+  def new
+    @memo = Memo.new
+  end
+
+  def create
+    @memo = Memo.new(memo_params)
+    if @memo.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def memo_params
+    params.require(:memo).permit(:text, :image, :present_id).merge(user_id: current_user.id)
+  end
 end
