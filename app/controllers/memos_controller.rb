48e1af2f43ec32e8d6b03ffb23085c57d02b6a80
samaimo <kii328815@gmail.com>
2024-04-25 21:01:16 +0900

    Update memos_controller.rb

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,5 +1,5 @@
 class MemosController < ApplicationController
-  # before_action :authenticate_user!, only: %i[create destroy]
+  before_action :authenticate_user!, only: %i[create destroy]
 
   def create
     @memo = Memo.new(memo_params)
